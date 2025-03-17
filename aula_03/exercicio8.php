291393ea11ad5af7b31143c30467d54c6277c792
EnzoCaetano015 <peracioenzo@gmail.com>
2025-03-17 08:47:14 -0300

    exercicios da aula 4

---
+++ aula_03/exercicio8.php
@@ -0,0 +1,16 @@
+<?php
+
+function gerarNumerosAleatorios() {
+    $numerosAleatorios = array();
+    
+    for ($i = 0; $i < 10; $i++) {
+        $numerosAleatorios[] = rand(1, 100);
+    }
+    
+    return $numerosAleatorios;
+}
+
+$numeros = gerarNumerosAleatorios();
+print_r($numeros);
+
+?>
